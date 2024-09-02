62ae4d4ea1ba387b73f2858903ec222f280e33de
Silk Rose <silkrose@love-tolerance.com>
2024-09-02 04:29:55 -0400

    Update tag SQL

--- fimficstats-rs/queries/create/tags.sql
+++ fimficstats-rs/queries/create/tags.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Tags (
-	id    integer NOT NULL PRIMARY KEY,
-	title text    NOT NULL,
-	type  text    NOT NULL,
-	text  text    NOT NULL,
-	href  text    NOT NULL
-)
+	id     integer NOT NULL PRIMARY KEY,
+	name   text    NOT NULL,
+	type   text    NOT NULL,
+	href   text    NOT NULL,
+	old_id text
+);
--- fimficstats-rs/queries/insert/tag.sql
+++ fimficstats-rs/queries/insert/tag.sql
@@ -1,8 +1,8 @@
-INSERT INTO Tags (id, title, TYPE, text, href)
+INSERT INTO Tags (id, name, type, href, old_id)
 VALUES
     (?1, ?2, ?3, ?4, ?5)
 ON CONFLICT(id) DO UPDATE SET
-	title = excluded.title,
-	TYPE = excluded.type,
-	text = excluded.text,
-	href = excluded.href;
+	name = excluded.name,
+	type = excluded.type,
+	href = excluded.href,
+	old_id = excluded.old_id;

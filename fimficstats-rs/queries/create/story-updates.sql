ad3d5c6f5471d8923250a1d09782adb6696f1eeb
PseudoBob Delightus <pseudobob.delightus@gmail.com>
2024-09-04 20:00:45 -0400

    Refactor columns and move prequel_id to stories table

--- fimficstats-rs/queries/create/story-updates.sql
+++ fimficstats-rs/queries/create/story-updates.sql
@@ -1,22 +1,21 @@
 CREATE TABLE IF NOT EXISTS Story_updates (
-	index             integer PRIMARY KEY autoincrement,
-	id                integer NOT NULL,
-	loop_index        integer NOT NULL,
-	date_modified     integer NOT NULL,
-	date_updated      integer NOT NULL,
-	date_published    integer NOT NULL,
-	views             integer NOT NULL,
-	total_views       integer NOT NULL,
-	num_comments      integer NOT NULL,
-	rating            integer NOT NULL,
-	completion_status text    NOT NULL,
-	likes             integer NOT NULL,
-	dislikes          integer NOT NULL,
-	prequel_id        integer,
+	id                    integer PRIMARY KEY autoincrement,
+	story_id              integer NOT NULL,
+	loop_index            integer NOT NULL,
+	date_modified         integer NOT NULL,
+	date_updated          integer NOT NULL,
+	date_published        integer NOT NULL,
+	views                 integer NOT NULL,
+	total_views           integer NOT NULL,
+	num_comments          integer NOT NULL,
+	rating                integer NOT NULL,
+	completion_status_id  integer NOT NULL,
+	likes                 integer NOT NULL,
+	dislikes              integer NOT NULL,
 
-		CONSTRAINT stories_author_id_fk FOREIGN KEY (author_id)
-			REFERENCES Authors (id),
-
-		CONSTRAINT story_index_id_fk FOREIGN KEY (id)
-			REFERENCES Story_index (story_id)
+	CONSTRAINT story_updates_story_id_fk FOREIGN KEY (id)
+		REFERENCES Stories (id),
+	
+	CONSTRAINT story_updates_completion_status_id_fk FOREIGN KEY (id)
+		REFERENCES Completion_status (id)
 )

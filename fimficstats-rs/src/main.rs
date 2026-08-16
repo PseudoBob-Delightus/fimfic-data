a4311aeb1ce876f74ae4b6e51ae35b68de5650cd
Silk Rose <silkrose@love-tolerance.com>
2026-08-16 09:40:18 -0400

    Update iteration log display

--- fimficstats-rs/src/main.rs
+++ fimficstats-rs/src/main.rs
@@ -245,10 +245,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 		tx.commit()?;
 
 		if !times.data.is_empty() {
+			let percent = (i as f64 / total as f64) * 100.0;
 			let average = times.average().unwrap();
 			println!(
-				"Iteration: {i:6}, ID: {id:6}, estimated time remaining: {}",
-				format_milliseconds((average * (total - i) as u32) as u128, Some(3))?
+				"Iteration: {i:6}, percentage: {percent:7.2}, ID: {id:6}, estimated time remaining: {}",
+				format_milliseconds((average * (total - i) as u32) as u128, Some(2))?
 			);
 		}
 
